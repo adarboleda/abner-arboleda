2e0c7e28483b7f8412e56a901072aaed60bcca36
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2025-03-19 10:57:13 -0500

    Base Corregida

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import freshBurger from "../../assets/fresh-burger.png";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h2 className="sectionTitle">Proyectos</h2> //arreglar
+      <h2 className="sectionTitle">Proyectos</h2>
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,15 @@
 import styles from "./SkillsStyles.module.css";
-import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import checkMarkIconDark from "../../assets/checkmark-dark.svg";
+import checkMarkIconLight from "../../assets/checkmark-light.svg";
 import SkillList from "../../common/SkillList";
+import { useTheme } from "../../common/ThemeContext";
 
 function Skills() {
+  const { theme } = useTheme();
+
+  const checkMarkIcon =
+    theme === "light" ? checkMarkIconLight : checkMarkIconDark;
+
   return (
     <section id="skills" className={styles.container}>
       <h2 className="sectionTitle">Skills</h2>
